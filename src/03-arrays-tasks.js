1e48a0df571ae3ab94c6bab5d75f2732111a8005
bshk102 <xartem.kx@gmail.com>
2022-12-09 20:35:25 +0500

    feat: 03 in process

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -234,8 +234,16 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  let last = arr[0];
+
+  return arr.map((item, i) => {
+    if (!i) {
+      return item;
+    }
+    last = item + last;
+    return last;
+  });
 }
 
 /**
@@ -249,8 +257,8 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((item, i) => i % 2);
 }
 
 /**
@@ -267,8 +275,12 @@ function getSecondItems(/* arr */) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.flatMap((item, i) => {
+    const a = new Array(i + 1);
+    a.fill(item, 0, i + 1);
+    return a;
+  });
 }
 
 /**
